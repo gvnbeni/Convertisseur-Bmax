0e1d27cef2c55d8b9dd61e5ccc1b3746750cb1ef
gvnbeni <benigavon17@gmail.com>
2025-07-16 17:45:37 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function convertir() {
+  const montant = parseFloat(document.getElementById("inputValue").value);
+  const from = document.getElementById("fromCurrency").value;
+  const to = document.getElementById("toCurrency").value;
+
+  if (isNaN(montant)) {
+    document.getElementById("resultat").textContent = "Montant invalide.";
+    return;
+  }
+
+  // Taux fixes
+  const taux = {
+    EUR: { USD: 1.1, CFA: 655.957 },
+    USD: { EUR: 0.91, CFA: 595 },
+    CFA: { EUR: 1 / 655.957, USD: 1 / 595 }
+  };
+
+  if (from === to) {
+    document.getElementById("resultat").textContent = "Même devise sélectionnée.";
+    return;
+  }
+
+  const valeurConvertie = montant * taux[from][to];
+  document.getElementById("resultat").textContent = 
+    `${montant} ${from} = ${valeurConvertie.toFixed(2)} ${to}`;
+}
+
+
